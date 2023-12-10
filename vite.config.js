2121700f2ab820f96c36a906f79984852b4c56a8
jbenoit <j_benoit81648@fanshaweonline.ca>
2023-12-10 17:53:05 -0500

    Imported process to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => { 
-  
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   return defineConfig({
-  plugins: [react()],
+    plugins: [react()],
     server: {
-    host: "0.0.0.0",
-    port: parseInt(process.env.PORT)
-  },
-})
+      host: "0.0.0.0",
+      port: parseInt(process.env.PORT)
+    },
+  })
 }
